878b5eb4c2be4634ba84e342c8a1fd880149bdf5
Witold <wandraszyk73@gmail.com>
2024-01-21 11:14:16 +0100

    Initial commit

---
+++ src/main/java/book/store/ecommerce/ZakupProduktu.java
@@ -0,0 +1,8 @@
+package book.store.ecommerce;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Zakup Produktu")
+public class ZakupProduktu extends ServletProcessApplication {
+}
